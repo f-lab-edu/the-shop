dcc5b3e0498a6e2113f6e391fc219d5e5774a507
Jiyun Park <jiyun991127@gmail.com>
2025-03-04 15:11:41 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/flab/theshop/TheshopApplication.java
@@ -0,0 +1,13 @@
+package com.flab.theshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/theshop/TheshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.theshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
