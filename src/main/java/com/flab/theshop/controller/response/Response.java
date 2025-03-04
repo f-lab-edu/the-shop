51ffeee8c1fb99148bb1ad4fb4473644b1a0e64d
Jiyun Park <jiyun991127@gmail.com>
2025-03-04 23:18:35 +0900

    #1 - 성공 응답 메세지 추가 및 공통 유효성 검증 로직 처리

--- src/main/java/com/flab/theshop/controller/response/Response.java
+++ src/main/java/com/flab/theshop/controller/response/Response.java
@@ -1,26 +1,52 @@
 package com.flab.theshop.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.flab.theshop.exception.ErrorCode;
+import com.flab.theshop.exception.FieldError;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.util.List;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
 import static org.springframework.http.HttpStatus.*;
 
 @AllArgsConstructor
+@Getter
 public class Response<T> {
 
     private int code;
+
     private String message;
+
+    @JsonInclude(NON_NULL) //null이면 응답에서 제외
     private T result;
 
+    @JsonInclude(NON_NULL)
+    private List<FieldError> errors;
+
+    // 성공 응답(데이터가 없는 경우)
     public static <T> Response<T> success() {
-        return new Response<T>(OK.value(), "SUCCESS", null);
+        return new Response<>(OK.value(), "SUCCESS", null, null);
+    }
+
+        public static <T> Response<T> success(String msg) {
+        return new Response<>(OK.value(), msg, null, null);
     }
 
     public static <T> Response<T> success(T result) {
-        return new Response<T>(OK.value(), "SUCCESS", result);
+        return new Response<>(OK.value(), "SUCCESS", result, null);
+    }
+
+    public static <T> Response<T> success(String msg, T result) {
+        return new Response<>(OK.value(), msg, result, null);
     }
 
     public static Response<Void> error(ErrorCode errorCode) {
-        return new Response<Void>(errorCode.getStatus().value(), errorCode.getMessage(), null);
+        return new Response<>(errorCode.getStatus().value(), errorCode.getMessage(), null, null);
+    }
+
+    public static Response<Void> error(ErrorCode errorCode, List<FieldError> errors) {
+        return new Response<>(errorCode.getStatus().value(), errorCode.getMessage(), null, errors);
     }
 }
--- src/main/java/com/flab/theshop/exception/GlobalControllerAdvice.java
+++ src/main/java/com/flab/theshop/exception/GlobalControllerAdvice.java
@@ -3,8 +3,11 @@ package com.flab.theshop.exception;
 import com.flab.theshop.controller.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
 
 import static com.flab.theshop.exception.ErrorCode.*;
 
@@ -17,6 +20,17 @@ public class GlobalControllerAdvice {
         log.error("오류 발생: ", e);
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(Response.error(e.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException e) {
+        log.error("잘못된 요청입니다: ", e);
+        ArrayList<FieldError> errors = new ArrayList<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.add(new FieldError(error.getField(), error.getDefaultMessage()));
+        });
+        return ResponseEntity.status(E400_INVALID.getStatus())
+                .body(Response.error(E400_INVALID, errors));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
